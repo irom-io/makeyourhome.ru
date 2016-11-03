a59096e53cdda7d6a604833bbd02ba8bc3ab5e74
Roman Orlovskiy <Ctuxu-R@ya.ru>
2016-11-03 21:31:51 +0300

    Added sub key

--- src/blocks/l10n/l10n.js
+++ src/blocks/l10n/l10n.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import l10n from './l10n.json';
 import {getLang} from 'blocks/page/__lang/page__lang';
+import objectPath from 'object-path';
+
+import l10nMain from './__main/l10n__main.json';
+import l10nMenu from './__menu/l10n__menu.json';
 
 class L10n extends React.Component {
     constructor(p_, context) {
         super(p_, context);
 
         this.getTranslate = this.getTranslate.bind(this);
-        this.state = {};
+        this.state = {
+            l10n: {
+                ...l10nMain,
+                ...l10nMenu
+            }
+        };
     }
     getTranslate() {
         const p_ = this.props;
+        const l10n = this.state.l10n;
         const lang = getLang();
 
-        let translate = l10n[p_.k];
+        let translate = objectPath.get(l10n, p_.k);
 
         if (translate[lang]) {
             translate = translate[lang];
