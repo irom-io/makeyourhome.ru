457d9d373fe2cf747d2ee711274a7327fc0fdaef
Roman Orlovskiy <Ctuxu-R@ya.ru>
2016-11-04 03:30:40 +0300

    Translate footer

--- src/blocks/page/__footer/page__footer.js
+++ src/blocks/page/__footer/page__footer.js
@@ -5,6 +5,7 @@ import text from 'blocks/text/text.css';
 import Auth from 'blocks/auth/auth';
 import Link from 'blocks/link/link';
 import item from 'blocks/item/item.css';
+import L10n from 'blocks/l10n/l10n';
 
 class PageFooter extends React.Component {
     constructor(p_) {
@@ -21,18 +22,18 @@ class PageFooter extends React.Component {
                 <div className={footer.content}>
                     <div className={`${grid.w25} ${grid.w30_tab} ${item.none_tabMini} ${text.underline}`}>
                         <Link>
-                            Конфиденциальность
+                            {L10n('confidentiality')}
                         </Link>
                     </div>
                     <div className={`${text.center} ${grid.w100_mob} ${grid.mbMini_mob}`}>
                         <div className={`${text.colored} ${text.bold} ${text.mdPlus} ${grid.mbMicro}`}>
                             <Link to="/">
-                                +7 (905) 668-20-61
+                                {L10n('phone')}
                             </Link>
                         </div>
                         <div className={text.normal}>
                             <Link to="/">
-                                Make your Home ©
+                                makeyourhome.ru ©
                             </Link>
                         </div>
                     </div>
