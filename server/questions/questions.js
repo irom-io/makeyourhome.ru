aadd70c94c1ae2e670b10df41a2432b1bd07e9a1
Roman Orlovskiy <Ctuxu-R@ya.ru>
2016-11-07 14:22:09 +0300

    Save question

--- server/questions/questions.js
+++ server/questions/questions.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const usersModel = require('../users/usersModel');
+const randomstring = require('randomstring');
 const mail = require('../mail/mail');
 
 const serverError = {error: {msg: 'serverError'}};
@@ -13,6 +14,24 @@ router.post('/', function(req, res) {
             subject: `Новый вопрос от пользователя ${user.login}`,
             body: req.body.question
         });
+    }
+
+    res.send({});
+});
+
+router.post('/add', function(req, res) {
+    var user = usersModel.get(req.body.user);
+
+    if (!user.error && user.isAdmin) {
+        var questions;
+        questions = fs.readFileSync(path.resolve(__dirname, './questions.json'), 'utf-8');
+        questions = JSON.parse(questions);
+        questions.unshift({
+            id: randomstring.generate(15),
+            question: req.body.question,
+            answer: req.body.answer
+        });
+        fs.writeFileSync(path.resolve(__dirname, './questions.json'), JSON.stringify(questions));
     }
 
     res.send({});
