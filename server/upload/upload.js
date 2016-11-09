6d190a99562dec7e26b8120a3dde18ca6711e0e1
Roman Orlovskiy <Ctuxu-R@ya.ru>
2016-11-09 18:06:51 +0300

    Upload with fetch

--- server/upload/upload.js
+++ server/upload/upload.js
@@ -2,12 +2,15 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({ dest: path.resolve(__dirname, './images/')});
+const upload = multer({ dest: path.resolve(__dirname, '../data/images/')});
 
 const serverError = {error: {msg: 'serverError'}};
 
+/*router.post('/', (req, res, next) => {
+    res.send({});
+});*/
+
 router.post('/', upload.array('images'), function(req, res) {
-    console.log(req.files);
     res.send({});
 });
 
