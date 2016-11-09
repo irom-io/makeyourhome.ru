d73b1d832ec505a955d36e75613d35af5a5f7606
Roman Orlovskiy <Ctuxu-R@ya.ru>
2016-11-09 18:34:02 +0300

    fileFilter

--- server/upload/upload.js
+++ server/upload/upload.js
@@ -2,15 +2,26 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({ dest: path.resolve(__dirname, '../data/images/')});
+const upload = multer({
+    fileFilter: function (req, file, cb) {
+        var filetypes = /jpeg|jpg|png/;
+        var mimetype = filetypes.test(file.mimetype);
+        var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+        if (mimetype && extname) {
+            return cb(null, true);
+        }
+        
+        cb("Error: File upload only supports the following filetypes - " + filetypes);
+    },
+    dest: path.resolve(__dirname, '../data/images/')
+});
 
 const serverError = {error: {msg: 'serverError'}};
 
-/*router.post('/', (req, res, next) => {
-    res.send({});
-});*/
-
 router.post('/', upload.array('images'), function(req, res) {
+    console.log(req.files);
+
     res.send({});
 });
 
