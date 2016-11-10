5591707f601cd907f9b1d5d38cf9626aa3feec8c
Roman Orlovskiy <Ctuxu-R@ya.ru>
2016-11-10 16:42:31 +0300

    collections images

--- src/pages/collections/collections.js
+++ src/pages/collections/collections.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TileLine from 'blocks/tile/_line/tile_line';
 import L10n from 'blocks/l10n/l10n';
+import {createSrc} from 'blocks/item/item';
 
 import page from 'blocks/page/page.css';
 
@@ -10,12 +11,12 @@ class Collections extends React.Component {
 
         this.state = {
             items: [
-                {src: require('./images/1.jpg'), key: 'house', link: {to: '/projects?collection=houses', key: 'house'}},
-                {src: require('./images/2.jpg'), key: 'bath', link: {to: '/projects?collection=bath', key: 'bath'}},
-                {src: require('./images/3.jpg'), key: 'garage', link: {to: '/projects?collection=garages', key: 'garage'}},
-                {src: require('./images/4.jpg'), key: 'recreation', link: {to: '/projects?collection=recreation', key: 'recreation'}},
-                {src: require('./images/5.jpg'), key: 'cabin', link: {to: '/projects?collection=cabin', key: 'cabin'}},
-                {src: require('./images/6.jpg'), key: 'other', link: {to: '/projects?collection=other', key: 'other'}}
+                {src: createSrc('collections/1.jpg'), key: 'house', link: {to: '/projects?collection=houses', key: 'house'}},
+                {src: createSrc('collections/2.jpg'), key: 'bath', link: {to: '/projects?collection=bath', key: 'bath'}},
+                {src: createSrc('collections/3.jpg'), key: 'garage', link: {to: '/projects?collection=garages', key: 'garage'}},
+                {src: createSrc('collections/4.jpg'), key: 'recreation', link: {to: '/projects?collection=recreation', key: 'recreation'}},
+                {src: createSrc('collections/5.jpg'), key: 'cabin', link: {to: '/projects?collection=cabin', key: 'cabin'}},
+                {src: createSrc('collections/6.jpg'), key: 'other', link: {to: '/projects?collection=other', key: 'other'}}
             ]
         };
     }
