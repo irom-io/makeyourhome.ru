8d3acf21103c2b6105a5598ebdd3f9aa2f612796
Roman Orlovskiy <Ctuxu-R@ya.ru>
2016-11-10 17:00:50 +0300

    styles fix

--- src/pages/styles/styles.js
+++ src/pages/styles/styles.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TileLine from 'blocks/tile/_line/tile_line';
 import L10n from 'blocks/l10n/l10n';
+import {createSrc} from 'blocks/item/item';
 
 import page from 'blocks/page/page.css';
 
@@ -10,13 +11,13 @@ class Styles extends React.Component {
 
         this.state = {
             items: [
-                {src: require('./images/1.jpg'), key: 'modern', link: {to: '/projects?style=modern', key: 'modern'}},
-                {src: require('./images/2.jpg'), key: 'classic', link: {to: '/projects?style=classic', key: 'classic'}},
-                {src: require('./images/3.jpg'), key: 'countryside', link: {to: '/projects?style=countryside', key: 'countryside'}},
-                {src: require('./images/4.jpg'), key: 'european', link: {to: '/projects?style=european', key: 'european'}},
-                {src: require('./images/5.jpg'), key: 'american', link: {to: '/projects?style=american', key: 'american'}},
-                {src: require('./images/6.jpg'), key: 'wooden', link: {to: '/projects?style=wooden', key: 'wooden'}},
-                {src: require('./images/7.jpg'), key: 'bungalow', link: {to: '/projects?style=bungalow', key: 'bungalow'}}
+                {src: createSrc('styles/1.jpg'), key: 'modern', link: {to: '/projects?style=modern', key: 'modern'}},
+                {src: createSrc('styles/2.jpg'), key: 'classic', link: {to: '/projects?style=classic', key: 'classic'}},
+                {src: createSrc('styles/3.jpg'), key: 'countryside', link: {to: '/projects?style=countryside', key: 'countryside'}},
+                {src: createSrc('styles/4.jpg'), key: 'european', link: {to: '/projects?style=european', key: 'european'}},
+                {src: createSrc('styles/5.jpg'), key: 'american', link: {to: '/projects?style=american', key: 'american'}},
+                {src: createSrc('styles/6.jpg'), key: 'wooden', link: {to: '/projects?style=wooden', key: 'wooden'}},
+                {src: createSrc('styles/7.jpg'), key: 'bungalow', link: {to: '/projects?style=bungalow', key: 'bungalow'}}
             ]
         };
     }
@@ -33,9 +34,8 @@ class Styles extends React.Component {
                             src={item.src}
                             text={L10n(`styles.${item.key}`)}
                             link={item.link}
-                        >
-                            <div dangerouslySetInnerHTML={{__html: L10n(`styles.${item.key}.descr`)}}></div>
-                        </TileLine>
+                            description={L10n(`styles.${item.key}.descr`)}
+                        />
                     )
                 })}
             </div>
