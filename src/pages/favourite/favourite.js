39becc8c1e915e8a2660c89243102aa5cf1a1c4b
Roman Orlovskiy <Ctuxu-R@ya.ru>
2016-11-11 22:42:26 +0300

    Save favourite post

--- src/pages/favourite/favourite.js
+++ src/pages/favourite/favourite.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import Layout from 'blocks/layout/layout';
 import Login from 'blocks/login/login';
+import Button from 'blocks/button/button';
+import TileLine from 'blocks/tile/_line/tile_line';
 import {getUser} from 'blocks/auth/auth';
 import L10n from 'blocks/l10n/l10n';
-//import {getLang} from 'blocks/page/__lang/page__lang';
+import {createSrc} from 'blocks/item/item';
+import {getLang} from 'blocks/page/__lang/page__lang';
 import api from 'blocks/api/api';
 
+import grid from 'blocks/grid/grid.css';
+import text from 'blocks/text/text.css';
+
+const step = 3;
 class Favourite extends React.Component {
     constructor(p_, context) {
         super(p_, context);
 
         this.state = {
             loading: false,
-            items: []
+            items: [],
+            visibleItems: step
         };
         this.loadFave = this.loadFave.bind(this);
     }
@@ -47,9 +55,13 @@ class Favourite extends React.Component {
                 }
             });
     }
+    seeMore() {
+        this.setState({visibleItems: (this.state.visibleItems + step)})
+    }
     render() {
         const s_ = this.state;
         const user = getUser();
+        const lang = getLang();
 
         return (
             <Layout
@@ -63,6 +75,46 @@ class Favourite extends React.Component {
                     onResponseAuth={(response) => this.onResponseAuth(response)}
                     onResponseRegistration={() => {this.setState({loading: false})}}
                 />
+                }
+
+                {user && s_.items &&
+                <div>
+                    {s_.items.map((item, index) => {
+                        if (index < s_.visibleItems) {
+                            let title = 'noTranslate';
+                            let shortText = 'noTranslate';
+
+                            if (item[lang]) {
+                                title = item[lang].title;
+                                shortText = item[lang].shortText;
+                            }
+
+                            return (
+                                <TileLine
+                                    key={`post_${index}`}
+                                    src={createSrc(item.images[0])}
+                                    text={title}
+                                    l10nText={true}
+                                    link={{to: `/posts/${item.id}`}}
+                                    description={shortText}
+                                />
+                            )
+                        } else {
+                            return '';
+                        }
+                    })}
+                </div>
+                }
+
+                {user && s_.items && (s_.visibleItems < s_.items.length) &&
+                <div className={`${text.center} ${grid.mtMini}`}>
+                    <Button
+                        onClick={() => this.seeMore()}
+                        className={grid.w100_mob}
+                    >
+                        {L10n('more')}
+                    </Button>
+                </div>
                 }
             </Layout>
         );
