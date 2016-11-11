2e2e8917c014538c1dd00ca37bd98f4f09449054
Roman Orlovskiy <Ctuxu-R@ya.ru>
2016-11-11 23:10:41 +0300

    Fave translate

--- src/pages/favourite/favourite.js
+++ src/pages/favourite/favourite.js
@@ -78,6 +78,9 @@ class Favourite extends React.Component {
 
                 {user && s_.items &&
                 <div>
+                    {(s_.items.length === 0) &&
+                    L10n('noFave')
+                    }
                     {s_.items.map((item, index) => {
                         if (index < s_.visibleItems) {
                             let title = 'noTranslate';
